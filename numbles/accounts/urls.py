32399e524bd24b2c6d264c346139971492530121
Nathan Osman <admin@quickmediasolutions.com>
2014-06-25 22:08:02 -0700

    Added accounts app to manage logging in and out.

---
+++ numbles/accounts/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, url
+from django.contrib.auth.views import login, logout
+from django.template.response import SimpleTemplateResponse
+
+def login_(request, **kwargs):
+    """Adds template variables to the login view."""
+    response = login(request, **kwargs)
+    if isinstance(response, SimpleTemplateResponse):
+        response.context_data.update({
+            'title': 'Login',
+        })
+    return response
+
+urlpatterns = patterns('numbles.accounts.views',
+
+    url(r'^login/$', login_, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout/$', logout, {'next_page':'accounts:login'}, name='logout'),
+)
