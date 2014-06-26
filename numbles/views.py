d5267d9de2c02f4d2a76d205d6bca51254b10c75
Nathan Osman <admin@quickmediasolutions.com>
2014-06-25 20:42:57 -0700

    Added sample template files.

--- numbles/views.py
+++ numbles/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def index(request):
     """Render the home page."""
     return render(request, 'index.html', {
-        'home': True,
+        'title': 'Dashboard',
     })
