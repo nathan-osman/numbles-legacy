14602c9eb89f04f852f1cd50430e05bcf2e57fad
Nathan Osman <admin@quickmediasolutions.com>
2014-07-01 23:04:23 -0700

    Created template tag for sidebar that displays all accounts.

--- numbles/ledger/templatetags/ledger.py
+++ numbles/ledger/templatetags/ledger.py
@@ -5,8 +5,8 @@ from numbles.ledger.models import Account
 register = template.Library()
 
 
-@register.inclusion_tag('ledger/account_list.html')
-def account_list():
+@register.inclusion_tag('ledger/account_list.html', takes_context=True)
+def account_list(context):
     return {
-        'accounts': Account.objects.all()
+        'accounts': Account.objects.filter(user=context['user']),
     }
