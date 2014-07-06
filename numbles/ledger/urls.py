425c94d18f323fef838cd0486bf77030ceebdeee
Nathan Osman <admin@quickmediasolutions.com>
2014-07-06 15:37:06 -0700

    Added basic URLs needed for manipulating transactions and accounts.

--- numbles/ledger/urls.py
+++ numbles/ledger/urls.py
@@ -2,10 +2,14 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('numbles.ledger.views',
 
-    url(r'^account/add/$', 'add_account', name='add_account'),
+    url(r'^account/add/$', 'edit_account', name='add_account'),
     url(r'^account/(?P<id>\d+)/$', 'view_account', name='view_account'),
+    url(r'^account/(?P<id>\d+)/edit/$', 'edit_account', name='edit_account'),
+    url(r'^account/(?P<id>\d+)/delete/$', 'delete_account', name='delete_account'),
 
-    url(r'^transaction/add/$', 'add_transaction', name='add_transaction'),
+    url(r'^transaction/add/$', 'edit_transaction', name='add_transaction'),
     url(r'^transaction/transfer/$', 'transfer', name='transfer'),
     url(r'^transaction/(?P<id>\d+)/$', 'view_transaction', name='view_transaction'),
+    url(r'^transaction/(?P<id>\d+)/edit/$', 'edit_transaction', name='edit_transaction'),
+    url(r'^transaction/(?P<id>\d+)/delete/$', 'delete_transaction', name='delete_transaction'),
 )
