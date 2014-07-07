dffee55b6192c2cccc7ee8d311480a5fadb3e754
Nathan Osman <admin@quickmediasolutions.com>
2014-07-06 17:37:05 -0700

    Added accumulate template tag for displaying a running balance.

--- numbles/ledger/templatetags/ledger.py
+++ numbles/ledger/templatetags/ledger.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django import template
 
 from numbles.ledger.models import Account
@@ -10,3 +12,11 @@ def account_list(context):
     return {
         'accounts': Account.objects.filter(user=context['user']),
     }
+
+
+@register.simple_tag(takes_context=True)
+def accumulate(context, name, value):
+    if name not in context:
+        context[name] = Decimal('0.00')
+    context[name] += value
+    return context[name]
