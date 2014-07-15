7aea9a01c194aa2bc9625ce7c1b3f782b4f7f194
Nathan Osman <admin@quickmediasolutions.com>
2014-07-14 21:38:11 -0700

    Completely reorganized the way transactions and accounts were stored in the database.

--- numbles/ledger/migrations/0001_initial.py
+++ numbles/ledger/migrations/0001_initial.py
@@ -8,20 +8,37 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'Total'
+        db.create_table(u'ledger_total', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+        ))
+        db.send_create_signal(u'ledger', ['Total'])
+
         # Adding model 'Account'
         db.create_table(u'ledger_account', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('total', self.gf('django.db.models.fields.related.ForeignKey')(related_name='accounts', to=orm['ledger.Total'])),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=40)),
             ('include_in_balance', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('balance', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=9, decimal_places=2)),
         ))
         db.send_create_signal(u'ledger', ['Account'])
+
+        # Adding model 'Year'
+        db.create_table(u'ledger_year', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('account', self.gf('django.db.models.fields.related.ForeignKey')(related_name='years', to=orm['ledger.Account'])),
+            ('year', self.gf('django.db.models.fields.PositiveSmallIntegerField')()),
+        ))
+        db.send_create_signal(u'ledger', ['Year'])
 
         # Adding model 'Transaction'
         db.create_table(u'ledger_transaction', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('account', self.gf('django.db.models.fields.related.ForeignKey')(related_name='transactions', to=orm['ledger.Account'])),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('year', self.gf('django.db.models.fields.related.ForeignKey')(related_name='transactions', to=orm['ledger.Year'])),
             ('date', self.gf('django.db.models.fields.DateTimeField')()),
             ('summary', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
@@ -33,8 +50,14 @@ class Migration(SchemaMigration):
 
 
     def backwards(self, orm):
+        # Deleting model 'Total'
+        db.delete_table(u'ledger_total')
+
         # Deleting model 'Account'
         db.delete_table(u'ledger_account')
+
+        # Deleting model 'Year'
+        db.delete_table(u'ledger_year')
 
         # Deleting model 'Transaction'
         db.delete_table(u'ledger_transaction')
@@ -79,22 +102,35 @@ class Migration(SchemaMigration):
         },
         u'ledger.account': {
             'Meta': {'object_name': 'Account'},
-            'balance': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '9', 'decimal_places': '2'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'include_in_balance': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'total': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'accounts'", 'to': u"orm['ledger.Total']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+        },
+        u'ledger.total': {
+            'Meta': {'object_name': 'Total'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
         },
         u'ledger.transaction': {
             'Meta': {'ordering': "('date',)", 'object_name': 'Transaction'},
-            'account': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'transactions'", 'to': u"orm['ledger.Account']"}),
             'amount': ('django.db.models.fields.DecimalField', [], {'max_digits': '9', 'decimal_places': '2'}),
             'date': ('django.db.models.fields.DateTimeField', [], {}),
             'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'linked': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ledger.Transaction']", 'null': 'True', 'blank': 'True'}),
             'reconciled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'summary': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+            'summary': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'year': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'transactions'", 'to': u"orm['ledger.Year']"})
+        },
+        u'ledger.year': {
+            'Meta': {'object_name': 'Year'},
+            'account': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'years'", 'to': u"orm['ledger.Account']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'year': ('django.db.models.fields.PositiveSmallIntegerField', [], {})
         }
     }
 
--- numbles/ledger/models.py
+++ numbles/ledger/models.py
@@ -3,39 +3,145 @@ from django.db import models
 from django.dispatch import receiver
 
 
-class Account(models.Model):
+# TODO: this should be a configurable setting
+MAX_DIGITS = 9
+DECIMAL_PLACES = 2
+
+
+class UpdateMixin:
+
+    UPDATE_FIELD = 'balance'
+
+    balance = models.DecimalField(max_digits=MAX_DIGITS,
+                                  decimal_places=DECIMAL_PLACES,
+                                  default=0)
+
+    def update(self):
+        """
+        Update the field with the sum of a related field.
+
+        UPDATE_QUERYSET must be set to the queryset that includes all rows to be
+        summed. UPDATE_FIELD is the name of the related field to be summed. If
+        UPDATE_PARENT is set, then it too will recalculate the sum.
+        """
+        data = self.UPDATE_QUERYSET().aggregate(sum=models.Sum(self.UPDATE_FIELD))
+        setattr(self, 'balance', data['sum'])
+        self.save()
+        if self.UPDATE_PARENT:
+            getattr(self, self.UPDATE_PARENT).update()
+
+
+class Total(models.Model, UpdateMixin):
+    """
+    The total balance for specific accounts chosen by the user.
+
+    The purpose of this class is to store the total balance of all accounts
+    where Account.include_in_balance is set to True.
+    """
+
+    UPDATE_QUERYSET = lambda self: self.accounts.filter(include_in_balance=True)
+    UPDATE_FIELD = 'balance'
+    UPDATE_PARENT = None
 
     user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.balance
+
+
+@receiver(models.signals.post_save, sender=User)
+def on_user_save(instance, created, **kwargs):
+    """
+    Create a Total instance whenever a user is created.
+    """
+    if created:
+        Total.objects.create(user=instance)
+
+
+class Account(models.Model, UpdateMixin):
+    """
+    A distinct set of transactions.
+    """
+
+    UPDATE_QUERYSET = lambda self: self.years.all()
+    UPDATE_FIELD = 'balance'
+    UPDATE_PARENT = 'total'
+
+    user = models.ForeignKey(User)
+    total = models.ForeignKey(Total, related_name='accounts')
 
     name = models.CharField(max_length=40,
                             help_text="Account name.")
     include_in_balance = models.BooleanField(default=False)
-
-    balance = models.DecimalField(max_digits=9,
-                                  decimal_places=2,
-                                  default=0)
 
     def __unicode__(self):
         return self.name
 
     @models.permalink
     def get_absolute_url(self):
-        return ('ledger:view_account', (), {
-            'id': self.id,
-        })
+        return ('ledger:view_account', (), {'id': self.id})
+
+
+@receiver(models.signals.post_delete, sender=Account)
+def on_account_delete(instance, **kwargs):
+    """
+    Remove the account's balance from the total balance.
+    """
+    if instance.include_in_balance:
+        instance.total.update()
+
+
+class Year(models.Model, UpdateMixin):
+    """
+    Transactions for a given year.
+    """
+
+    UPDATE_QUERYSET = lambda self: self.transactions.all()
+    UPDATE_FIELD = 'amount'
+    UPDATE_PARENT = 'year'
+
+    user = models.ForeignKey(User)
+    account = models.ForeignKey(Account, related_name='years')
+
+    year = models.PositiveSmallIntegerField()
+
+    def __unicode__(self):
+        return unicode(self.year)
+
+    def update(self):
+        """
+        Self destruct if no longer needed.
+        """
+        if not self.transactions.count():
+            self.delete()
+        else:
+            super(Year, self).update()
+
+
+@receiver(models.signals.post_delete, sender=Year)
+def on_year_delete(instance, **kwargs):
+    """
+    Remove the year's balance from the account balance.
+    """
+    instance.account.update()
 
 
 class Transaction(models.Model):
+    """
+    An amount transferred from or to an account.
+    """
 
-    account = models.ForeignKey(Account, related_name='transactions')
+    user = models.ForeignKey(User)
+    year = models.ForeignKey(Year, related_name='transactions')
 
     date = models.DateTimeField(help_text="Date and time of the transaction.")
     summary = models.CharField(max_length=100,
                                help_text="Brief description of the transaction.")
     description = models.TextField(blank=True,
                                    help_text="Additional details or information.")
-    amount = models.DecimalField(max_digits=9,
-                                 decimal_places=2,
+
+    amount = models.DecimalField(max_digits=MAX_DIGITS,
+                                 decimal_places=DECIMAL_PLACES,
                                  help_text="Amount of the transaction.")
     reconciled = models.BooleanField(default=False)
 
@@ -58,31 +164,42 @@ class Transaction(models.Model):
 
 
 @receiver(models.signals.post_init, sender=Transaction)
-def store_original_amount(instance, **kwargs):
+def on_transaction_init(instance, **kwargs):
     """
-    Store the original amount of the transaction.
-    This is necessary so that the difference can be calculated when the
-    transaction is modified or deleted.
+    Store the original year and amount of the transaction.
     """
-    instance.original_amount = instance.amount or 0
+    instance.original_year = instance.date.year
+    instance.original_amount = instance.amount
 
 
 @receiver(models.signals.post_save, sender=Transaction)
-def update_account_balance_post_save(instance, **kwargs):
+def on_transaction_save(instance, created, **kwargs):
     """
-    Update the transaction's account balance.
-    The account stores the sum of all transactions. By calculating the
-    difference of the transaction from its previous value, the sum will
-    continue to reflect the correct sum.
+    Update the transaction's year and account balance.
+
+    If the year has changed, check to see if the old year no longer contains any
+    transactions. If so, delete it. Also, the new year might not exist, so
+    create it if this is the case.
     """
-    instance.account.balance = models.F('balance') + (instance.amount - instance.original_amount)
-    instance.account.save()
+    changed = instance.original_year != instance.date.year
+    if not created and changed:
+        Year.objects.get(
+            user=instance.user,
+            account=None,
+            year=instance.original_year
+        ).save()
+    if created or changed:
+        year, _ = Year.objects.get_or_create(
+            user=instance.user,
+            account=None,
+            year=instance.date.year
+        )
+        year.update()
 
 
 @receiver(models.signals.post_delete, sender=Transaction)
-def update_account_balance_post_delete(instance, **kwargs):
+def on_transaction_delete(instance, **kwargs):
     """
-    Remove the transaction amount from the account balance.
+    Remove the transaction's amount from the year's balance.
     """
-    instance.account.balance = models.F('balance') - instance.amount
-    instance.account.save()
+    instance.year.update()
