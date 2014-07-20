dad7f1352ee8e1074e1515630bc586f10f19ffb0
Nathan Osman <admin@quickmediasolutions.com>
2014-07-20 14:00:27 -0700

    Added user profile model.

---
+++ numbles/accounts/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.dispatch import receiver
+from pytz import common_timezones
+
+
+class Profile(models.Model):
+    """
+    Storage for user preferences.
+    """
+
+    user = models.OneToOneField(
+        User,
+        primary_key=True
+    )
+
+    timezone = models.CharField(
+        max_length=40,
+        choices=[(t, t) for t in common_timezones],
+        default='UTC',
+        help_text="Used for displaying all times."
+    )
+
+    def __unicode__(self):
+        return unicode(self.user)
+
+
+@receiver(models.signals.post_save, sender=User)
+def on_user_save(instance, created, **kwargs):
+    """
+    Create a Profile instance whenever a user is created.
+    """
+    if created:
+        Profile.objects.create(user=instance)
