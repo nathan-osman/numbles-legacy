911c375b912e87360e2dbc750dda88b3103ee68e
Nathan Osman <nathan@quickmediasolutions.com>
2015-01-04 12:21:56 -0800

    Added django_archive to requirements.txt.

--- numbles/settings.py
+++ numbles/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Django helper applications
+    'django_archive',
     'widget_tweaks',
     # Numbles applications
     'numbles.accounts',
