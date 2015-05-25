4a85d0f7e15b1f2e943f3a41236fe29fdd04cf8e
Nathan Osman <nathan@quickmediasolutions.com>
2015-05-24 23:06:01 -0700

    Added missing templatetag.

---
+++ numbles/accounts/templatetags/gravatar.py
@@ -0,0 +1,20 @@
+from hashlib import md5
+
+from django import template
+from django.utils.encoding import smart_bytes
+
+register = template.Library()
+
+
+@register.filter()
+def gravatar(email, size=64):
+    """
+    Return the Gravatar URL for the provided email.
+    :param email: the email address to hash
+    :param size: the size of the Gravatar to generate
+    :returns: the URL of the image
+    """
+    return 'http://gravatar.com/avatar/%s?d=identicon&size=%s' % (
+        md5(smart_bytes(email)).hexdigest(),
+        size,
+    )
