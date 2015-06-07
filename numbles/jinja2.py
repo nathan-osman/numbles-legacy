8f6c91e1924cf885dfe8a8af4cea7359ae604a52
Nathan Osman <nathan@quickmediasolutions.com>
2015-06-07 16:33:34 -0700

    Added MD5 to the Jinja2 environment.

--- numbles/jinja2.py
+++ numbles/jinja2.py
@@ -3,6 +3,7 @@ Customize the Jinja2 environment
 """
 
 from __future__ import absolute_import
+from hashlib import md5
 
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse
@@ -11,10 +12,11 @@ from jinja2 import Environment
 
 def environment(**kwargs):
     """
-    Add the static() and url() functions to the Jinja2 environment
+    Add some utility functions to the Jinja2 environment
     """
     env = Environment(**kwargs)
     env.globals.update({
+        'md5': lambda x: md5(x).hexdigest(),
         'static': staticfiles_storage.url,
         'url': reverse,
     })
