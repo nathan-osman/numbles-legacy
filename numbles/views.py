43698aed6ead084329903709b85e7d3b651024c4
Nathan Osman <nathan@quickmediasolutions.com>
2015-11-12 13:52:26 -0800

    Fixed errors with home page.

--- numbles/views.py
+++ numbles/views.py
@@ -1,8 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.db.models import Sum
 from django.shortcuts import render
-
-from numbles.ledger.models import Account, Transaction, Year
 
 
 @login_required
@@ -10,12 +7,4 @@ def index(request):
     return render(request, 'pages/index.html', {
         'title': 'Dashboard',
         'home': True,
-        'accounts': Account.objects.filter(user=request.user),
-        'transactions': Transaction.objects.filter(user=request.user).order_by('-date')[:6],
-        'years': Year.objects.filter(user=request.user, account__include_in_balance=True)
-            .values('year').annotate(sum=Sum('balance')),
-        'stats': {
-            'total_accounts': Account.objects.filter(user=request.user).count(),
-            'total_transactions': Transaction.objects.filter(user=request.user).count(),
-        }
     })
