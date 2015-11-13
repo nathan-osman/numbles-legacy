3b6629eb352e1dab8a6fd3c11ce8371cfb57a23e
Nathan Osman <nathan@quickmediasolutions.com>
2015-11-13 12:22:55 -0800

    Added proper linebreaks to description field.

--- numbles/jinja2.py
+++ numbles/jinja2.py
@@ -7,6 +7,7 @@ from hashlib import md5
 
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse
+from django.template.defaultfilters import linebreaksbr
 from django.utils.timezone import template_localtime
 from jinja2 import Environment
 from widget_tweaks.templatetags.widget_tweaks import add_class, widget_type
@@ -22,6 +23,7 @@ def environment(**kwargs):
         'widget_type': widget_type,
     })
     env.globals.update({
+        'linebreaksbr': linebreaksbr,
         'localtime': lambda x: template_localtime(x).strftime('%Y-%m-%d %H:%M:%S'),
         'md5': lambda x: md5(x).hexdigest(),
         'static': staticfiles_storage.url,
