3a18e1edad2252e6095f0182651cc51044fe7166
Nathan Osman <nathan@quickmediasolutions.com>
2015-11-13 20:08:00 -0800

    Added transaction list button to home page.

--- numbles/jinja2.py
+++ numbles/jinja2.py
@@ -9,7 +9,7 @@ from os.path import basename
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse
 from django.template.defaultfilters import linebreaksbr
-from django.utils.timezone import template_localtime
+from django.utils.timezone import now, template_localtime
 from jinja2 import Environment
 from widget_tweaks.templatetags.widget_tweaks import add_class, widget_type
 
@@ -28,6 +28,7 @@ def environment(**kwargs):
         'linebreaksbr': linebreaksbr,
         'localtime': lambda x: template_localtime(x).strftime('%Y-%m-%d %H:%M:%S'),
         'md5': lambda x: md5(x).hexdigest(),
+        'now': now,
         'static': staticfiles_storage.url,
         'url': reverse,
     })
